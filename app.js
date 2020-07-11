0470324d58f8d9b33b0c51dd9b5fc504d6e8f476
Oliver Carlsson <oliver@framecore.se>
2020-07-11 22:36:14 +0200

    Password Validator V1 :tada:

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Script for style
+// ** Script for style **
 const eye = document.querySelector(".eye")
 const input = $($(".eye").attr("toggle"));
 
@@ -40,62 +40,121 @@ $('.container').on('mousedown', function(e) {
 
 
 
-// Script for function
+// ** Script for info box **
+let clickInfoBox = true;
+
+$(".fa-info").click(function() {
+  showInfoBox()
+  // Fire hideInfoBox after 5 seconds
+  setTimeout(hideInfoBox, 5000)
+});
+
+function showInfoBox() {
+  if (clickInfoBox === true) {
+    clickInfoBox = false;
+    // Shows Element after 1 second
+    $("#info-box").removeClass("hide-element")
+    $("#info-box").animate({
+      opacity: 1
+    }, 250);
+  } 
+}
+
+function hideInfoBox() {
+  // hides element after 1 sec
+  $("#info-box").animate({
+    opacity: 0
+  }, 500);
+  // Removes element fully after 1 sec
+  setTimeout(function() {
+    clickInfoBox = true;
+    $("#info-box").addClass("hide-element")
+  }, 500)          
+}
+
+
+
+
+// ** Script for validation funciton **
 let userPassword;
-let accepted = 0;
+let acceptedLength = false;
+let acceptedNumber = false;
+let acceptedUppercase = false;
+let acceptedSpecial = false;
+let acceptedSpace = false;
 // When clicking submit check if it hits minimum requiments
 $("#enter").click(function() {
   userPassword = $("#password-input").val()
 
+  showInfoBox()
+
   // Check password LENGTH
-  if ($("#password-input").val().length >= 5 && $("#password-input").val().length <= 128) {
-    console.log("Accepted")
-    accepted++;
+  if ($("#password-input").val().length >= 8 && $("#password-input").val().length <= 128) {
+    console.log("Accepted Length")
+    acceptedLength = true;
+    $("#validation-length").css("color", "green")
   } else {
-    console.log("Password too weak")
+    $("#validation-length").css("color", "red")
+    console.log("Needs length")
+    acceptedLength = false;
   }
 
   // Check password number
   if (/\d/.test(userPassword) === true) {
-    console.log("Accepted")
-    accepted++;
+    $("#validation-number").css("color", "green")
+    console.log("Accepted number")
+    acceptedNumber = true;
   } else {
-    console.log("Password too weak")
+    $("#validation-number").css("color", "red")
+    console.log("Needs number")
+    acceptedNumber = false;
   }
 
   // Check password UPPERCASE LETTER
   if (/[A-Z]/.test(userPassword) === true) {
-    console.log("Accepted")
-    accepted++;
+    $("#validation-uppercase").css("color", "green")
+    console.log("Accepted uppercase letter")
+    acceptedUppercase = true;
   } else {
-    console.log("Password too weak")
+    $("#validation-uppercase").css("color", "red")
+    console.log("needs uppercase letter")
+    acceptedUppercase = false;
   }
 
   // Check password SPECIAL character
   if (/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(userPassword) === true) {
-    console.log("Accepted")
-    accepted++;
+    $("#validation-special").css("color", "green")
+    console.log("Accepted special character")
+    acceptedSpecial = true;
   } else {
-    console.log("Password too weak")
+    $("#validation-special").css("color", "red")
+    console.log("needs special character")
+    acceptedSpecial = false;
   }
 
   // Check password can't contain Spaces
   if (/ /.test(userPassword) === true) {
-    accepted--;
+    $("#validation-spaces").css("color", "red")
+    acceptedSpace = false;
     console.log("Can't contain spaces")
+  } else {
+    $("#validation-spaces").css("color", "green")
+    acceptedSpace = true;
   }
 
-  if (accepted === 4) {
+  if (acceptedLength === true && acceptedNumber === true && acceptedUppercase === true && acceptedSpecial === true && acceptedSpace === true) {
     $(".container").css("border", "2px solid green")
+    $("#enter").css("border", "2px solid green")
+    $("#enter").css({
+      "border": "2px solid green",
+      "border-top": "2px solid rgba(0, 0, 0, 0)"
+    })
   } else {
     $(".container").css("border", "2px solid red")
+    $("#enter").css({
+      "border": "2px solid red",
+      "border-top": "2px solid rgba(0, 0, 0, 0)"
+    })
+
   }
-
 });
-
-
-
-
-
-
-
